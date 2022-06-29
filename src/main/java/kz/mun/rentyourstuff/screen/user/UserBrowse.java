507571b5ee3164da788e85a8b08b6eeb3816fbf6
Alexandr Mun <mun_a2006@yahoo.de>
2022-06-29 13:47:37 +0600

    initial commit

---
+++ src/main/java/kz/mun/rentyourstuff/screen/user/UserBrowse.java
@@ -0,0 +1,12 @@
+package kz.mun.rentyourstuff.screen.user;
+
+import kz.mun.rentyourstuff.entity.User;
+import io.jmix.ui.navigation.Route;
+import io.jmix.ui.screen.*;
+
+@UiController("rys_User.browse")
+@UiDescriptor("user-browse.xml")
+@LookupComponent("usersTable")
+@Route("users")
+public class UserBrowse extends StandardLookup<User> {
+}
